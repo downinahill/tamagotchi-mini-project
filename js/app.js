97a4eb40c1860354c5443d41cce554bfd25ad5bf
downinahill <brian.hill.0023@gmail.com>
2021-08-03 06:32:51 -0400

    Created tamagotchi class

---
+++ js/app.js
@@ -0,0 +1,17 @@
+class Tamagotchi {
+    constructor(name, species, age){
+        this.name = name
+        this.species = species
+        this.age = age
+    }
+    Eat() {
+
+    }
+    Sleep() {
+
+    }
+    Play() {
+        
+    }
+
+}
